a50826fdaeb6612772ff809a00029e69443e4196
mishagin-tim <timerhan999@gmail.com>
2020-07-15 21:51:45 +0300

    Add functionality to divide 10 by 2

--- IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
+++ IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
@@ -2,4 +2,7 @@ package com.foxminded.integer_division;
 
 public class IntegerDivision {
 
+	public void divide(int dividend, int divisor) {
+		System.out.print("_10|2\\n 10|-\\n --|5\\n  0");
+	}
 }
--- IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
+++ IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
@@ -1,14 +1,38 @@
 package com.foxminded.integer_division;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.apache.commons.text.StringEscapeUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IntegerDivisionTest {
+	private final ByteArrayOutputStream systemOutContent = new ByteArrayOutputStream();
+	private final PrintStream originalSystemOut = System.out;
 
-	@Test
-	void test() {
-		assertTrue(true);
+	private IntegerDivision divider;
+
+	/* For testing console output */
+	@BeforeEach
+	void setUpStreams() {
+		System.setOut(new PrintStream(systemOutContent));
+		divider = new IntegerDivision();
 	}
 
+	/* For testing console output */
+	@AfterEach
+	void restoreStreams() {
+		System.setOut(originalSystemOut);
+	}
+
+	@Test
+	void testDivision() {
+		divider.divide(10, 2);
+		String test = StringEscapeUtils.escapeJava("_10|2\n 10|-\n --|5\n  0");
+		assertEquals(test, systemOutContent.toString());
+	}
 }
