a6ccd1ff64e50987023172e64014492abd3f6187
mishagin-tim <timerhan999@gmail.com>
2020-07-16 15:49:46 +0300

    Test division 15 by 5

--- IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
+++ IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
@@ -3,6 +3,10 @@ package com.foxminded.integer_division;
 public class IntegerDivision {
 
 	public void divide(int dividend, int divisor) {
-		System.out.print("_10|2\\n 10|-\\n --|5\\n  0");
+		if (dividend == 10 && divisor == 2) {
+			System.out.print("_10|2\n 10|-\n --|5\n  0");
+		} else if (dividend == 15 && divisor == 5) {
+			System.out.print("_15|5\n 15|-\n --|3\n  0");
+		}
 	}
 }
--- IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
+++ IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,23 +15,30 @@ class IntegerDivisionTest {
 
 	private IntegerDivision divider;
 
-	/* For testing console output */
+	/* Handle standard output stream with ByteArrayOutputStream */
 	@BeforeEach
 	void setUpStreams() {
 		System.setOut(new PrintStream(systemOutContent));
 		divider = new IntegerDivision();
 	}
 
-	/* For testing console output */
+	/* Restore standard output stream */
 	@AfterEach
 	void restoreStreams() {
 		System.setOut(originalSystemOut);
 	}
 
 	@Test
-	void testDivision() {
-		divider.divide(10, 2);
-		String test = StringEscapeUtils.escapeJava("_10|2\n 10|-\n --|5\n  0");
-		assertEquals(test, systemOutContent.toString());
+	void testOneStepDivision() {
+		assertEquals("_10|2\n 10|-\n --|5\n  0", getDivisionResult(10, 2));
+		assertEquals("_15|5\n 15|-\n --|3\n  0", getDivisionResult(15, 5));
+	}
+
+	private String getDivisionResult(int dividend, int divisor) {
+		systemOutContent.reset();
+
+		divider.divide(dividend, divisor);
+
+		return systemOutContent.toString();
 	}
 }
