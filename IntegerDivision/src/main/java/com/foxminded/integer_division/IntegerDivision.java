c9b1344683534ac124b74d327a978126bc643ffc
mishagin-tim <timerhan999@gmail.com>
2020-07-16 16:03:09 +0300

    Refactor divide

--- IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
+++ IntegerDivision/src/main/java/com/foxminded/integer_division/IntegerDivision.java
@@ -3,10 +3,12 @@ package com.foxminded.integer_division;
 public class IntegerDivision {
 
 	public void divide(int dividend, int divisor) {
-		if (dividend == 10 && divisor == 2) {
-			System.out.print("_10|2\n 10|-\n --|5\n  0");
-		} else if (dividend == 15 && divisor == 5) {
-			System.out.print("_15|5\n 15|-\n --|3\n  0");
-		}
+		String template = "_%d|%d\n %d|-\n --|%d\n  %d";
+		int quotient = dividend / divisor;
+		int reminder = dividend % divisor;
+
+		String result = String.format(template, dividend, divisor, divisor * quotient, quotient, reminder);
+
+		System.out.print(result);
 	}
 }
