587e1442bbd5423a4c889e9b86921120e20fb6b5
mishagin-tim <timerhan999@gmail.com>
2020-07-17 00:02:12 +0300

    Add test of division one-digit number by three-digit number

--- IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
+++ IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
@@ -38,6 +38,7 @@ class IntegerDivisionTest {
 		assertEquals("_101|25\n 100|--\n ---|4\n   1", getDivisionResult(101, 25));
 		assertEquals("_543|500\n 500|---\n ---|1\n  43", getDivisionResult(543, 500));
 		assertEquals("_43|500\n  0|---\n --|0\n 43", getDivisionResult(43, 500));
+		assertEquals("_4|500\n 0|---\n -|0\n 4", getDivisionResult(4, 500));
 	}
 
 	private String getDivisionResult(int dividend, int divisor) {
