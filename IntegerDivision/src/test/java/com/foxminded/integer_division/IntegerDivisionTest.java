f7dfbc8444d183ac39d803d937d35d9a038fa1ae
mishagin-tim <timerhan999@gmail.com>
2020-07-23 13:27:41 +0300

    Refactor tests: change names, split into small tests.

--- IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
+++ IntegerDivision/src/test/java/com/foxminded/integer_division/IntegerDivisionTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 class IntegerDivisionTest {
 	private final ByteArrayOutputStream systemOutContent = new ByteArrayOutputStream();
 	private final PrintStream originalSystemOut = System.out;
-
+	
 	/* Handle standard output stream with ByteArrayOutputStream */
 	@BeforeEach
 	void setUpStreams() {
@@ -28,143 +28,194 @@ class IntegerDivisionTest {
 
 	@Test
 	void divideShouldThrowIllegalArgumentExceptionWhenDivisorIsZero() {
-		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-			getDivisionResult(123, 0);
-		});
-		assertEquals(exception.getMessage(), "Divisor can't be zero");
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> getDivisionResult(123, 0));
+		
+		String expected = "Divisor can't be zero";
+
+		String actual = exception.getMessage();
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void integerDivisionShouldReturnOneStepDivisionWithZeroQuotientWhenDividendIsZero() {
+		String expected = "_0|123\n" + 
+						  " 0|---\n" + 
+						  " -|0\n" + 
+						  " 0";
+
+		String actual = getDivisionResult(0, 123);
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void integerDivisionShouldReturnOneByOneDivisionRepresentationWhenOneIsDividedByOne() {
+		String expected = "_1|1\n" + 
+		 		  " 1|-\n" + 
+		 		  " -|1\n" + 
+		 		  " 0";
+
+		String actual = getDivisionResult(1, 1);
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void IntegerDivisionShouldReturnOneStepDivisionRepresentationWhenDividendIsLessThanDivisor() {
+		String expected = "_3|12\n" + 
+				   " 0|--\n" + 
+				   " -|0\n" + 
+				   " 3";
+
+		String actual = getDivisionResult(3, 12);
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void integerDivisionShouldReturnOneStepDivisionRepresentationWhenDivisionIsOneStep() {
+
+		String expected = "_5|3\n" + 
+				   " 3|-\n" + 
+				   " -|1\n" + 
+				   " 2";
+
+		String actual = getDivisionResult(5, 3);
+
+		assertEquals(expected, actual);
+
+
+		expected = "_26|12\n" + 
+				   " 24|--\n" + 
+				   " --|2\n" + 
+				   "  2";
+
+		actual = getDivisionResult(26, 12);
+
+		assertEquals(expected, actual);
+		
+
+		expected = "_543|500\n" + 
+				   " 500|---\n" + 
+				   " ---|1\n" + 
+				   "  43";
+
+		actual = getDivisionResult(543, 500);
+
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testOneStepDivision() {
+	void integerDivisionShouldReturnTwoStepDivisionRepresentationWhenDivisionIsTwoStep() {
+		String expected = "_145|5\n" + 
+				 		  " 10 |--\n" + 
+				 		  " -- |29\n" + 
+				 		  " _45\n" + 
+				 		  "  45\n" + 
+				 		  "  --\n" + 
+				 		  "   0";
 
-		assertEquals("_1|1\n" + 
-					 " 1|-\n" + 
-					 " -|1\n" + 
-					 " 0", getDivisionResult(1, 1));
+		String actual = getDivisionResult(145, 5);
 
-		assertEquals("_3|12\n" + 
-					 " 0|--\n" + 
-					 " -|0\n" + 
-					 " 3", getDivisionResult(3, 12));
+		assertEquals(expected, actual);
 		
-		assertEquals("_5|3\n" + 
-					 " 3|-\n" + 
-					 " -|1\n" + 
-					 " 2", getDivisionResult(5, 3));
 
-		assertEquals("_11|3\n" + 
-					 "  9|-\n" + 
-					 " --|3\n" + 
-					 "  2", getDivisionResult(11, 3));
+		expected = "_144|12\n" + 
+				   " 12 |--\n" + 
+				   " -- |12\n" + 
+				   " _24\n" + 
+				   "  24\n" + 
+				   "  --\n" + 
+				   "   0";
+
+		actual = getDivisionResult(144, 12);
+
+		assertEquals(expected, actual);
 		
-		assertEquals("_26|12\n" + 
-					 " 24|--\n" + 
-					 " --|2\n" + 
-					 "  2", getDivisionResult(26, 12));
 
-		assertEquals("_543|500\n" + 
-					 " 500|---\n" + 
-					 " ---|1\n" + 
-					 "  43", getDivisionResult(543, 500));
+		expected = "_1024|32\n" + 
+				   "  96 |--\n" + 
+				   " --- |32\n" + 
+				   "  _64\n" + 
+				   "   64\n" + 
+				   "   --\n" + 
+				   "    0";
+
+		actual = getDivisionResult(1024, 32);
+
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testTwoStepDivision() {
-		assertEquals("_145|5\n" + 
-					 " 10 |--\n" + 
-					 " -- |29\n" + 
-					 " _45\n" + 
-					 "  45\n" + 
-					 "  --\n" + 
-					 "   0", getDivisionResult(145, 5));
+	void integerDivisionShouldReturnThreeStepDivisionRepresentationWhenDivisionIsThreeStep() {
+		String expected = "_1245|5\n" + 
+				   " 10  |---\n" + 
+				   " --  |249\n" + 
+				   " _24\n" + 
+				   "  20\n" + 
+				   "  --\n" + 
+				   "  _45\n" + 
+				   "   45\n" + 
+				   "   --\n" + 
+				   "    0";
 
-		assertEquals("_144|12\n" + 
-					 " 12 |--\n" + 
-					 " -- |12\n" + 
-					 " _24\n" + 
-					 "  24\n" + 
-					 "  --\n" + 
-					 "   0", getDivisionResult(144, 12));
+		String actual = getDivisionResult(1245, 5);
+
+		assertEquals(expected, actual);
 		
-		assertEquals("_1024|32\n" + 
-					 "  96 |--\n" + 
-					 " --- |32\n" + 
-					 "  _64\n" + 
-					 "   64\n" + 
-					 "   --\n" + 
-					 "    0", getDivisionResult(1024, 32));
-	}
 
-	@Test
-	void testMultipleStepDivision() {
-		assertEquals("_1245|5\n" + 
-					 " 10  |---\n" + 
-					 " --  |249\n" + 
-					 " _24\n" + 
-					 "  20\n" + 
-					 "  --\n" + 
-					 "  _45\n" + 
-					 "   45\n" + 
-					 "   --\n" + 
-					 "    0",
-				getDivisionResult(1245, 5));
+		expected = "_135151512|241\n" + 
+				   " 1205     |------\n" +
+				   " ----     |560794\n" + 
+				   " _1465\n" + 
+				   "  1446\n" + 
+				   "  ----\n" + 
+				   "   _1915\n" + 
+				   "    1687\n" + 
+				   "    ----\n" + 
+				   "    _2281\n" + 
+				   "     2169\n" + 
+				   "     ----\n" + 
+				   "     _1122\n" + 
+				   "       964\n" + 
+				   "      ----\n" + 
+				   "       158";
+
+		actual = getDivisionResult(135151512, 241);
+
+		assertEquals(expected, actual);
 		
-		assertEquals("_135151512|241\n" + 
-					 " 1205     |------\n" +
-					 " ----     |560794\n" + 
-					 " _1465\n" + 
-					 "  1446\n" + 
-					 "  ----\n" + 
-					 "   _1915\n" + 
-					 "    1687\n" + 
-					 "    ----\n" + 
-					 "    _2281\n" + 
-					 "     2169\n" + 
-					 "     ----\n" + 
-					 "     _1122\n" + 
-					 "       964\n" + 
-					 "      ----\n" + 
-					 "       158", getDivisionResult(135151512, 241));
 
-		assertEquals("_8742965|1334\n" + 
-					 " 8004   |----\n" +
-					 " ----   |6553\n" + 
-					 " _7389\n" + 
-					 "  6670\n" + 
-					 "  ----\n" + 
-					 "  _7196\n" + 
-					 "   6670\n" + 
-					 "   ----\n" + 
-					 "   _5265\n" + 
-					 "    4002\n" + 
-					 "    ----\n" + 
-					 "    1263", getDivisionResult(8742965, 1334));
+		expected = "_123456789|7\n" + 
+				   "  7       |--------\n" + 
+				   " --       |17636684\n" + 
+				   " _53\n" + 
+				   "  49\n" + 
+				   "  --\n" + 
+				   "  _44\n" + 
+				   "   42\n" + 
+				   "   --\n" + 
+				   "   _25\n" + 
+				   "    21\n" + 
+				   "    --\n" + 
+				   "    _46\n" + 
+				   "     42\n" + 
+				   "     --\n" + 
+				   "     _47\n" + 
+				   "      42\n" + 
+				   "      --\n" + 
+				   "      _58\n" + 
+				   "       56\n" + 
+				   "       --\n" + 
+				   "       _29\n" + 
+				   "        28\n" + 
+				   "        --\n" + 
+				   "         1";
 
-		assertEquals("_123456789|7\n" + 
-					 "  7       |--------\n" + 
-					 " --       |17636684\n" + 
-					 " _53\n" + 
-					 "  49\n" + 
-					 "  --\n" + 
-					 "  _44\n" + 
-					 "   42\n" + 
-					 "   --\n" + 
-					 "   _25\n" + 
-					 "    21\n" + 
-					 "    --\n" + 
-					 "    _46\n" + 
-					 "     42\n" + 
-					 "     --\n" + 
-					 "     _47\n" + 
-					 "      42\n" + 
-					 "      --\n" + 
-					 "      _58\n" + 
-					 "       56\n" + 
-					 "       --\n" + 
-					 "       _29\n" + 
-					 "        28\n" + 
-					 "        --\n" + 
-					 "         1", getDivisionResult(123456789, 7));
+		actual = getDivisionResult(123456789, 7);
+
+		assertEquals(expected, actual);
 	}
 
 	private String getDivisionResult(int dividend, int divisor) {
